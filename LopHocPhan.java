7c4c0705669f37a03e0e38bac3897b478d08d873
Hongthao2003 <125128510+Hongthao2003@users.noreply.github.com>
2023-04-26 23:29:09 +0700

    Create LopHocPhan.java

---
+++ LopHocPhan.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labth1;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class LopHocPhan {
+   private String maLHP, tenLHP, tenGV, thongtinLopHoc, dsSV;
+   
+    ArrayList<LopHocPhan> hp = new ArrayList<>();
+   Scanner sc = new Scanner(System.in);
+   
+    public LopHocPhan() {
+    }
+
+    public LopHocPhan(String maLHP, String tenLHP, String tenGV, String thongtinLopHoc, String dsSV) {
+        this.maLHP = maLHP;
+        this.tenLHP = tenLHP;
+        this.tenGV = tenGV;
+        this.thongtinLopHoc = thongtinLopHoc;
+        this.dsSV = dsSV;
+    }
+
+    public String getMaLHP() {
+        return maLHP;
+    }
+
+    public String getTenLHP() {
+        return tenLHP;
+    }
+
+    public String getTenGV() {
+        return tenGV;
+    }
+
+    public String getThongtinLopHoc() {
+        return thongtinLopHoc;
+    }
+
+    public String getDsSV() {
+        return dsSV;
+    }
+
+    public void setMaLHP(String maLHP) {
+        this.maLHP = maLHP;
+    }
+
+    public void setTenLHP(String tenLHP) {
+        this.tenLHP = tenLHP;
+    }
+
+    public void setTenGV(String tenGV) {
+        this.tenGV = tenGV;
+    }
+
+    public void setThongtinLopHoc(String thongtinLopHoc) {
+        this.thongtinLopHoc = thongtinLopHoc;
+    }
+
+    public void setDsSV(String dsSV) {
+        this.dsSV = dsSV;
+    }
+
+
+    }
