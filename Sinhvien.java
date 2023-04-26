cd11dea06e7c2935c44f65fcc768f94c237adc1b
Hongthao2003 <125128510+Hongthao2003@users.noreply.github.com>
2023-04-27 00:15:21 +0700

    Update Sinhvien.java

--- Sinhvien.java
+++ Sinhvien.java
@@ -44,35 +44,5 @@ public class Sinhvien {
     public String toString() {
         return "Sinhvien{" + "maSV=" + maSV + ", hoTen=" + hoTen + '}';
     }
-     private ArrayList<Sinhvien> svn = new ArrayList<>();
-    Scanner sc = new Scanner(System.in);
     
-    public void in_sv(){
-        for (Sinhvien sinhVien : svn) {
-            System.out.println(sinhVien.toString());
-        }
-    }
-    
-    public List<Sinhvien> nhapthongtinsv(){
-        List<Sinhvien> dsSV = new ArrayList<>();
-        System.out.println("Nhap thong tin cua sinh vien");
-        
-        while(true){
-            System.out.println("Nhap ma sinh vien: ");
-            String maSV = sc.nextLine();
-            
-            System.out.println("Nhap ho ten sinh vien: ");
-            String hoTen = sc.nextLine();
-            
-            if(maSV.isEmpty()||hoTen.isEmpty()){
-                System.out.println("Nhap thanh cong");
-                break;
-            }
-            Sinhvien sv = new Sinhvien(maSV, hoTen);
-        }
-        return dsSV;
-    }
-        public int getSoLuongSV(){
-        return svn.size();
-    }
         }
