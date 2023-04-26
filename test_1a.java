7020374bded8d4f265dcf9be981787a95911af26
Hongthao2003 <125128510+Hongthao2003@users.noreply.github.com>
2023-04-26 21:41:54 +0700

    Update test_1a.java

--- test_1a.java
+++ test_1a.java
@@ -9,53 +9,49 @@ import java.util.Scanner;
  * @author LENOVO
  */
 public class test_1a {
-
-    private static int menu_1;
+ public static void main(String[]args){
+     DSXE ds = new DSXE();
         Scanner sc = new Scanner(System.in);
-        public void Nhap(){
-             
-        }
-        
-       public static void main(String[]args){
-           Vehicle v[]=null;
-           int a,n=0, menu;
-           boolean flag =true;
+        int menu;
            do{
                System.out.println("Menu lua chon chuc nang---------------------------");
                System.out.println("1. Nhập thông tin và tạo các đối tượng xe1, xe2, x3");
                System.out.println("2. Xuất bảng kê khai tiền thuế trước bạ của các xe ");
                System.out.println("3. Thoát");
                System.out.print("Nhap: ");
+               menu = sc.nextInt();
                
-             switch (menu_1){
-              case 1: {
-                 System.out.print("Nhập số lượng xe bạn muốn khai báo thuế: ");
-                    v= new Vehicle[n];
-                    for (int i = 0; i < n; i++) {
-                        System.out.println("Xe thứ " + (i+1));
-                        v[i] = new Vehicle();
-                  
-                    }
-                    break;
-              }
-                    case 2: {
-                    System.out.printf("%5s %5s %5s %10s %10s %8s \n","Mã xe","Chủ xe","Dung tích","Trị giá","mô tả","Thuế");
-                    for (int i = 0; i < n; i++) {
-                        v[i].thuexe();
-                    }
-                    break;
-                 }
-               default:
-                   {
-                       if (menu_1 == 0) {
-                           System.out.println("Thoat chuong trinh");
-                       } else {
-                           System.out.println("Vui long nhap lai lua chon");
-                       }
-                   }
+             switch (menu){
+              case 1: 
+                  sc.nextLine();
+                    System.out.print("Nhap ten chu xe: ");
+                    String TenChuXe = sc.nextLine();
+                    
 
+                    System.out.print("Nhap loai xe: ");
+                    String LoaiXe = sc.nextLine();
+                    
+                    System.out.print("Nhap gia tri xe: ");
+                    double TriGia = sc.nextDouble();
+                    
+                    System.out.print("Nhap dung tich xe: ");
+                    double DungTich = sc.nextDouble();
+                    double thuexe = ds.thuecuaxe(TriGia, menu);
+                    
+                    Vehicle vh = new Vehicle(LoaiXe, menu, menu, menu, menu);
+                    ds.them_xe(vh);
+                    break;
+                case 2:
+                    ds.Xuat();
+                    break;
+
+                default:
+                    if(menu !=0){
+                        System.out.println("Nhap lai chuc nang");
+                    }else{
+                        System.out.println("Thoat chuong trinh thanh cong");
+                    }
             }
-        } while (menu_1 != 0);
-       
+        }while(menu != 0);
     }
 }
